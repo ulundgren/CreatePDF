765ec92861e0b47b114bf6edd7552a7868f75e99
Ulf Lundgren <uffe@ulflundgren.se>
2025-10-10 17:48:51 +0200

    PDF signing in SelectPDF

--- PdfApi/SelectPDF.cs
+++ PdfApi/SelectPDF.cs
@@ -7,17 +7,31 @@ internal class SelectPDFService : IPDFService
     public byte[] CreatePDF(string content, string? baseUrl)
     {
 
-
         // instantiate a html to pdf converter object
-        HtmlToPdf converter = new HtmlToPdf();
+        SelectPdf.HtmlToPdf converter = new SelectPdf.HtmlToPdf();
 
         // create a new pdf document converting the html code
-        PdfDocument doc = converter.ConvertHtmlString(content, baseUrl);
+        SelectPdf.PdfDocument doc = converter.ConvertHtmlString(content, baseUrl);
+        // PdfPage page = doc.AddPage();
+        // // get the #PKCS12 certificate from file
+        // PdfDigitalCertificatesCollection certificates =
+        //     PdfDigitalCertificatesStore.GetCertificates(@"uc-client.p12", "test");
+        // PdfDigitalCertificate certificate = certificates[0];
+
+        // PdfRenderingResult result;
+        // PdfFont font = doc.AddFont(PdfStandardFont.Helvetica);
+        // font.Size = 20;
+        // PdfTextElement txt = new PdfTextElement(0, 0, "This is a text element", font);
+        // result = page.Add(txt);
 
 
-
-
-
+        // // create the digital signature object
+        // PdfDigitalSignatureElement signature =
+        //     new PdfDigitalSignatureElement(result.PdfPageLastRectangle, certificate);
+        // signature.Reason = "SelectPdf";
+        // signature.ContactInfo = "SelectPdf";
+        // signature.Location = "SelectPdf";
+        // page.Add(signature);
 
         // save pdf document
         byte[] d = doc.Save();
